9702d1f8b8497a1f7c839eb16d9096aa645f1ec6
xavier <xavier@gandillot.com>
2017-05-09 18:21:51 +0200

    MongoDriver (syncroneous) utility added.

---
+++ src/main/java/com/twiceagain/mymongodriver/drivers/Mongos.java
@@ -0,0 +1,89 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.twiceagain.mymongodriver.drivers;
+
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.client.ListDatabasesIterable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+import org.bson.Document;
+
+/**
+ * Factory class to provide access to a SYNC MongoClient.
+ *
+ * @author xavier
+ */
+public class Mongos {
+
+    private static final Logger LOG = Logger.getLogger(Mongos.class.getName());
+
+    /**
+     * Protected constructor prevents initialisation.
+     */
+    protected Mongos() {
+    }
+
+    /**
+     * Internal, singleton instance for client.
+     */
+    protected static MongoClient client = null;
+    protected static String connection = "mongodb://localhost:27017";
+
+    public static MongoClient getClient() {
+        if (client == null) {
+            client = new MongoClient(new MongoClientURI(connection));
+        }
+        return client;
+    }
+
+    /**
+     * Set the connection string to use, and reset the client. Will throw on
+     * invalid string, or timeout on no connection.
+     *
+     * @param aConnection
+     */
+    public static void setConnection(String aConnection) {
+        if (aConnection != null) {
+            if (!connection.equals(aConnection)) {
+                // Close client, to force new client with new connection details.
+                close();
+                connection = aConnection;
+                // Reopen client and access for early failure detection
+                getClient().listDatabases().first();
+            }
+
+        }
+    }
+
+    /**
+     * Get the list of databases names.
+     *
+     * @return
+     */
+    public static List<String> listDatabaseNames() {
+        List<String> res = new ArrayList<>();
+
+        for (String name : getClient().listDatabaseNames()) {
+            res.add(name);
+        }
+        return res;
+    }
+
+    /**
+     * Closes existing client, and frees all resources. Not necessary, since
+     * client is a singleton, unless there was a change in the connection
+     * string.
+     */
+    public static void close() {
+        if (client != null) {
+            client.close();
+            client = null;
+        }
+    }
+
+}
---
+++ src/test/java/com/twiceagain/mymongodriver/drivers/MongosTest.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.twiceagain.mymongodriver.drivers;
+
+import com.mongodb.MongoTimeoutException;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ *
+ * @author xavier
+ */
+public class MongosTest {
+    
+    public MongosTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void defaultConnectionAndListDatabases() {
+        Mongos.setConnection("mongodb://localhost:27017");
+        System.out.printf("\nMongo databases :\n%s\n", Mongos.listDatabaseNames());
+    }
+    
+    @Test(expected = IllegalArgumentException.class)
+    public void invalidConnectionStringShouldThrow() {
+        // Invalid strings will throw
+        Mongos.setConnection("invalidString");        
+    }
+    
+    
+    @Test(expected = MongoTimeoutException.class)
+    public void unknownPortConnectionStringShouldTimeout() {
+        // Invalid port will time out with no other exception thrown
+         Mongos.setConnection("mongodb://localhost:27777");          
+    }
+    
+}
